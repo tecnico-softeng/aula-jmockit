87a1968f5c6a655115d8ddf107c34045418e7e98
António Rito Silva <Rito.Silva@ist.utl.pt>
2017-03-07 14:09:33 +0000

    jmockit initial

--- broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/AdventureProcessMethodMockTest.java
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/AdventureProcessMethodMockTest.java
@@ -1,12 +1,13 @@
 package pt.ulisboa.tecnico.softeng.broker.domain;
 
 import org.joda.time.LocalDate;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import mockit.Expectations;
-import mockit.Injectable;
 import mockit.Mocked;
 import mockit.integration.junit4.JMockit;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
@@ -16,37 +17,49 @@ import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
 
 @RunWith(JMockit.class)
 public class AdventureProcessMethodMockTest {
+	private static final String ACTIVITY_REFERENCE = "activityReference";
+	private static final String HOTEL_REFERENCE = "hotelReference";
+	private static final String PAYMENT_CONFIRMATION = "paymentConfirmation";
+	private static final String IBAN = "BK01987654321";
 	private final LocalDate begin = new LocalDate(2016, 12, 19);
 	private final LocalDate end = new LocalDate(2016, 12, 21);
-
-	@Injectable
 	private Broker broker;
 
+	@Before
+	public void setUp() {
+		this.broker = new Broker("BR98", "Travel Light");
+	}
+
 	@Test
-	public void processWithNoExceptions(@Mocked BankInterface bankInterface, @Mocked HotelInterface hotelInterface,
-			@Mocked ActivityInterface activityInterface) {
+	public void processWithNoExceptions(@Mocked final BankInterface bankInterface,
+			@Mocked final HotelInterface hotelInterface, @Mocked final ActivityInterface activityInterface) {
 		new Expectations() {
 			{
-				BankInterface.processPayment("BK01987654321", 300);
-				this.result = "paymentConfirmation";
+				BankInterface.processPayment(IBAN, 300);
+				this.result = PAYMENT_CONFIRMATION;
 
 				HotelInterface.reserveHotel(Type.SINGLE, AdventureProcessMethodMockTest.this.begin,
 						AdventureProcessMethodMockTest.this.end);
-				this.result = "hotelReference";
+				this.result = HOTEL_REFERENCE;
 
 				ActivityInterface.reserveActivity(AdventureProcessMethodMockTest.this.begin,
 						AdventureProcessMethodMockTest.this.end, 20);
-				this.result = "activityReference";
+				this.result = ACTIVITY_REFERENCE;
 			}
 		};
 
-		Adventure adventure = new Adventure(this.broker, this.begin, this.end, 20, "BK01987654321", 300);
+		Adventure adventure = new Adventure(this.broker, this.begin, this.end, 20, IBAN, 300);
 
 		adventure.process();
 
-		Assert.assertEquals("paymentConfirmation", adventure.getBankPayment());
-		Assert.assertEquals("hotelReference", adventure.getRoomBooking());
-		Assert.assertEquals("activityReference", adventure.getActivityBooking());
+		Assert.assertEquals(PAYMENT_CONFIRMATION, adventure.getBankPayment());
+		Assert.assertEquals(HOTEL_REFERENCE, adventure.getRoomBooking());
+		Assert.assertEquals(ACTIVITY_REFERENCE, adventure.getActivityBooking());
+	}
+
+	@After
+	public void tearDown() {
+		Broker.brokers.clear();
 	}
 
 }
